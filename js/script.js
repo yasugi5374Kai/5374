545820d29248419f5f3bba7f4be0a9b2eec92bf7
matsueT5374 <mishima001@users.noreply.github.com>
2017-11-16 14:12:41 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -308,7 +308,10 @@ var TrashModel = function(_lable, _cell, remarks) {
             window.alert(kubun + '①：' + sDate);
             window.alert(kubun + '②：' + FrBPDay);
 
-            //if  (sDate() == FrBPDay
+            if (sDate == FrBPDay) {
+                window.alert(kubun + '③：' + FrBPDay);
+
+            }
 
             //◇
             //window.alert(kubun + '：' + this.mostRecent);
