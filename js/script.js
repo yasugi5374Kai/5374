c2041c92bdec8704acb84c28300a9a2908edb856
matsueT5374 <mishima001@users.noreply.github.com>
2017-12-14 10:58:06 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -684,14 +684,23 @@ $(function() {
     $(".accordion-body").on("shown.bs.collapse", function() {
       var body = $('body');
       var accordion_offset = $($(this).parent().get(0)).offset().top;
+
+
+      //◆ body.animate({
+      //◆   scrollTop: accordion_offset
+      //◆ }, 50);
+
+
       body.animate({
-        scrollTop: accordion_offset
       }, 50);
+
+
     });
     //アコーディオンの非表示部分をクリックしたら
     $(".accordion-body").on("hidden.bs.collapse", function() {
       if ($(".in").length == 0) {
-        $("html, body").scrollTop(0);
+        //◆ $("html, body").scrollTop(0);
+
       }
     });
   }
