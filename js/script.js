464ede8bf770c010141311f0dd910aa33113db07
matsueT5374 <mishima001@users.noreply.github.com>
2017-12-15 15:14:03 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -413,6 +413,8 @@ var RemarkModel = function(data) {
 $(function() {
 /*   windowHeight = $(window).height(); */
 
+
+
   var center_data = new Array();
   var descriptions = new Array();
   var areaModels = new Array();
@@ -740,6 +742,35 @@ $(function() {
       }
     });
   }
+
+
+
+function(){
+  // 固定する場所が存在することの確認
+  if( $('.fixing-base .fixing-box').length > 0 ){
+    var baseSelector = '.fixing-base'
+    var fixingSelector = baseSelector + ' .fixing-box'
+
+    $(window).on('load scroll resize', function(){
+      var baseTop = $(baseSelector).offset().top
+
+      //固定開始位置より後にスクロールされた場合
+      if( $(window).scrollTop() > baseTop ){
+        $(fixingSelector).addClass('fixed')
+        $(baseSelector).height($(fixingSelector).outerHeight())
+        $(fixingSelector).width($(baseSelector).width())
+
+      //固定開始位置以前にスクロールされた場合
+      } else {
+        $(fixingSelector).removeClass('fixed')
+        $(baseSelector).height('')
+        $(fixingSelector).width('')
+      }
+    })
+  }
+}
+
+
 
   function onChangeSelect(group_name, area_name) {
     if (group_name == -1) {
